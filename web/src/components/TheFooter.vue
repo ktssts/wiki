8e0f9a2ba6128a98737fdd664fc8cf2a7e39e4c9
ktssts <ub.songwenxu@gmail.com>
2022-01-02 16:34:26 +0900

    4-7 制作TheFooter组件

---
+++ web/src/components/TheFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Wiki电子书1111
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheFooter',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
