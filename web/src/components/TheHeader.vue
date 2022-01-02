56f1725ec5bab4161f5bc4db8536e282462fcf38
ktssts <ub.songwenxu@gmail.com>
2022-01-02 16:29:06 +0900

    4-7 制作TheHeader组件

---
+++ web/src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <a-layout-header class="header">
+      <div class="logo" />
+      <a-menu
+          v-model:selectedKeys="selectedKeys1"
+          theme="dark"
+          mode="horizontal"
+          :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">nav 10000</a-menu-item>
+        <a-menu-item key="2">nav 2</a-menu-item>
+        <a-menu-item key="3">nav 3</a-menu-item>
+      </a-menu>
+    </a-layout-header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheHeader',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
